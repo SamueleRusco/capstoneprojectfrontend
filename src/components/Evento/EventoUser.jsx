c9a5c8b95675cb3a2fba033d7b9650617a3899c3
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2023-05-29 18:20:53 +0200

    sistemati eventi (modale)

--- src/components/Evento/EventoUser.jsx
+++ src/components/Evento/EventoUser.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Card, Row } from "react-bootstrap";
+import { Button, Card, Modal, ModalBody, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import moment from 'moment';
 
 const EventoUser = () => {
   const bearerTokenNum = useSelector(
@@ -8,6 +9,21 @@ const EventoUser = () => {
   );
   const [eventi, setEventi] = useState([]);
   const [fileGet, setFileGet] = useState([]);
+ const [showModal, setShowModal] = useState(new Array(eventi.length).fill(false));
+
+
+const openModal = (index) => {
+  const updatedShowModal = [...showModal];
+  updatedShowModal[index] = true;
+  setShowModal(updatedShowModal);
+};
+
+
+const closeModal = (index) => {
+  const updatedShowModal = [...showModal];
+  updatedShowModal[index] = false;
+  setShowModal(updatedShowModal);
+};
 
   const getImage = async () => {
     try {
@@ -53,23 +69,59 @@ const EventoUser = () => {
     return null;
   };
 
+  const dateConverter = (element) =>{
+      const moment = require('moment');
+  const date = moment(element.dataEvento);
+  const giorno = date.format('DD'); // Restituisce il nome del giorno della settimana
+  const giornoName = date.format('dddd')
+  const mese = date.format('MMMM'); // Restituisce il nome completo del mese
+  const anno = date.format('YYYY'); // Restituisce l'anno come una stringa
+
+  return (
+    <div className="dateSquare" style={{margin:"0", padding:"0"}}>
+      <span>{mese}</span>
+      <p style={{fontSize:"3.5em", padding:"0", margin:"0"}}>{giorno}</p>
+      
+    </div>
+  );
+
+  }
+
   useEffect(() => {
-    //console.log("sono did mount");
-    fetchData();
+  fetchData();
   }, []);
 
   return (
     <Row>
       {eventi &&
-        eventi.map((element, i) => (
-          <div key={i} style={{margin:"10px"}}>
-          <Card  className="eventCard">
-            {renderImmagine(element.immagineEvento)}
+        eventi.map((element, index) => (
+  <div key={index} style={{ margin: "10px" }}>
+    <Card className="eventCard" onClick={() => openModal(index)}>
+            {dateConverter(element)}
+            {/* {renderImmagine(element.immagineEvento)} */}
+            
             <Card.Body style={{textAlign:"start"}}>
               <Card.Title>{element.titoloEvento}</Card.Title>
+              <Card.Text className="clickMeDetails">Click Me for Details ⬆️</Card.Text>
               <Card.Text className="eventDesc">{element.descrizione}</Card.Text>
             </Card.Body>
           </Card>
+          <Modal show={showModal[index]} onHide={() => closeModal(index)}>
+  <Modal.Header closeButton>
+    <Modal.Title>{element.titoloEvento}</Modal.Title>
+  </Modal.Header>
+  {renderImmagine(element.immagineEvento)}
+  <Modal.Body>
+    <p>{element.descrizione}</p>
+    <p>artisti:</p>
+  {element.artistiEvento.map((a, index) => {
+      return <span key={index}>{a}</span>; // Aggiunta della keyword "return" per restituire il valore
+    })}
+  </Modal.Body>
+  <Modal.Footer>
+    
+  </Modal.Footer>
+</Modal>
           </div>
         ))}
     </Row>
